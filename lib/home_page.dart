c9f133b457a42513dc502a8087e36f6560f45793
Santosh Bhandari <santoshvandari1@gmail.com>
2024-10-12 11:58:05 +0545

    initial

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Share Hisab"),
+        ),
+        body: const Text("Hello, World!"),
+      ),
+    );
+  }
+}
